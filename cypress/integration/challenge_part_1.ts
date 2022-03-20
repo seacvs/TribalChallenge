f709fb7d47e6f80e6aebbc5daa5530ff8da934dd
Sebastian Aceves <sebastian@altomobile.io>
2022-03-20 16:50:42 -0600

    Clean code
    clean code, formatting, remove unused auto-created folders

--- cypress/integration/challenge_part_1.ts
+++ cypress/integration/challenge_part_1.ts
@@ -1,35 +1,31 @@
-/// <reference types ="cypress" /> 
-import { result } from "cypress/types/lodash"
-import {Careers} from "../pages/careers"
+/// <reference types ="cypress" />
+import { Careers } from "../pages/careers";
 
-let tribalUrl = 'https://boards.greenhouse.io/tribalcredit'
-const careers = new Careers()
+let tribalUrl = "https://boards.greenhouse.io/tribalcredit";
+const careers = new Careers();
 
-it('Challenge test part I', () => {
-    //1 Go to https://boards.greenhouse.io/tribalcredit
-    careers.navigate(tribalUrl)
-    
-    //2. Check the correct page is opened.
-    careers.validateUrl(tribalUrl)
+it("Challenge test part I", () => {
+  //1 Go to https://boards.greenhouse.io/tribalcredit
+  careers.navigate(tribalUrl);
 
-    //3. Filter by “Engineering” from the “Department” from the select dropdownlist.
-    cy.get('#s2id_departments-select').click()
-    cy.get('#select2List0 > li').filter(':contains("Engineering")').click()
-    
-    //4. Filter by “Mexico” from the “Office” dropdownlist.
-    cy.get('#s2id_offices-select').click()
-    cy.get('#select2List1 > li').filter(':contains("Mexico")').click()
+  //2. Check the correct page is opened.
+  careers.validateUrl(tribalUrl);
 
-    //5. Check “Senior QA Engineer” is displayed.
-        //cy.get(':nth-child(27) > a').click()
-        //PREVIOUS cy.get('#main > :nth-child(12) > .opening').should('contain', 'Mexico').and('contain',position)
+  //3. Filter by “Engineering” from the “Department” from the select dropdownlist.
+  cy.get("#s2id_departments-select").click();
+  cy.get("#select2List0 > li").filter(':contains("Engineering")').click();
 
-    cy.get('.level-0 > .opening').filter(':contains("Mexico")')
-   
+  //4. Filter by “Mexico” from the “Office” dropdownlist.
+  cy.get("#s2id_offices-select").click();
+  cy.get("#select2List1 > li").filter(':contains("Mexico")').click();
+
+  //5. Check “Senior QA Engineer” is displayed.
+  cy.get(".level-0 > .opening")
+    .filter(':contains("Mexico")')
+
     //Expected to Fail, the request string "Senior QA Engineer" don't exist for Mexico it should be "Sr. QA Engineer"
-    .filter(':contains("Senior QA Engineer")')
-    
-    //Uncomment line below to find the QA position for Mexico Senior and comment line above
-    //.filter(':contains("Sr. QA Engineer")')
-    
-})
+    .filter(':contains("Senior QA Engineer")');
+
+  //Uncomment line below to find the QA position for Mexico Senior and comment line above
+  //.filter(':contains("Sr. QA Engineer")')
+});
--- cypress/pages/careers.ts
+++ cypress/pages/careers.ts
@@ -1,22 +1,18 @@
 export class Careers {
-        //1.1 Go to https://boards.greenhouse.io/tribalcredit
-        navigate(url : string){
-            cy.visit(url)
-        }
+  //1.1 Go to https://boards.greenhouse.io/tribalcredit
+  navigate(url: string) {
+    cy.visit(url);
+  }
 
-        //1.2 Check the correct page is opened.
-        validateUrl(url: string){
-            cy.url().should('eq', url)
-        }
+  //1.2 Check the correct page is opened.
+  validateUrl(url: string) {
+    cy.url().should("eq", url);
+  }
 
-
-        //2.1 check dropdown elements are displayed and can be selected
-        checkDropdownElements(){
-            cy.get('.select2-results > li > .select2-result-label')
-            .should(($items) => {
-                expect($items).to.be.visible
-        })         
-    }
-
-
+  //2.1 check dropdown elements are displayed and can be selected
+  checkDropdownElements() {
+    cy.get(".select2-results > li > .select2-result-label").should(($items) => {
+      expect($items).to.be.visible;
+    });
+  }
 }
